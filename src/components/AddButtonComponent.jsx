a1772b88add82689a138dca758975bf4a3e20cd7
Molnar-Zoltan <weperdev@gmail.com>
2025-02-23 04:58:37 +0100

    Add 'cursor-pointer' class to all buttons

--- src/components/AddButtonComponent.jsx
+++ src/components/AddButtonComponent.jsx
@@ -14,7 +14,7 @@ const AddButtonComponent = () => {
     }
 
     return (
-        <button onClick={AddTask} className="bg-orange-600 hover:bg-orange-700 active:bg-orange-800 fill-neutral-100 active:fill-orange-200 transition-colors px-2 py-0.5 rounded-sm font-bold text-2xl">
+        <button onClick={AddTask} className="bg-orange-600 hover:bg-orange-700 active:bg-orange-800 fill-neutral-100 active:fill-orange-200 transition-colors px-2 py-0.5 rounded-sm font-bold text-2xl cursor-pointer">
             <svg className="w-14" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M427.67-427.67H172v-104.66h255.67v-256.34h104.66v256.34h256.34v104.66H532.33V-172H427.67v-255.67Z"/></svg>
         </button>
     );
